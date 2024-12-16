d1f6b13d9636479c24057ee712ef533c8c1ed8a5
lujinhong <ljhn1829@corp.netease.com>
2024-12-16 17:22:41 +0800

    add log

--- examples/run_regression_movielens.py
+++ examples/run_regression_movielens.py
@@ -29,6 +29,7 @@ if __name__ == "__main__":
     # 3.generate input data for model
     train, test = train_test_split(data, test_size=0.2)
     train_model_input = {name: train[name] for name in feature_names}
+    # print("train_model_input:", train_model_input.shape())
     test_model_input = {name: test[name] for name in feature_names}
     # 4.Define Model,train,predict and evaluate
 
